d8df392cabbd7a7d3db22e9376b3fa71204c5ecc
LeonPakzad <44465654+LeonPakzad@users.noreply.github.com>
2024-03-29 15:25:17 +0100

    added some crud functions

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ a tool to use open street map locations in your own project
 
 ### Database Configurations
 1. alter your DATABASE_URL
-2. npx prisma migrate dev --name init
+2. ```npx prisma migrate dev --name init```
+
+after changing:
+```npx prisma migrate dev``` 
---
+++ index.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    getPlacesAll();
+    addPlace();
+    updatePlace("changed_mail@test.com");
+    getPlacesAll();
+    deletePlacesAll();
+}
+
+async function addPlace()
+{
+    await prisma.place.create({
+
+        data: {
+            fk_place: 12,
+            node: 123,
+            name: 'Alice',
+            email: 'place@test.com',
+            website: 'www.place.com',
+            opening_hours: "Fr: 12:00-18:00",
+            lat: 1.0,
+            long: 1.0,        
+            addr_city: 'test',   
+            addr_housenumber: 1,
+            addr_postcode: 11111,
+            addr_Street: 'teststreet',
+            
+            cuisine: 'italian',
+            diet_vegan: false,
+            diet_vegetarian: false
+        },
+    })
+}
+
+async function getPlacesAll()
+{
+    const allPlaces = await prisma.place.findMany();
+    console.log(allPlaces);
+}
+
+async function deletePlacesAll()
+{
+    const allPlaces = await prisma.place.deleteMany();
+    console.log(allPlaces);
+}
+
+async function updatePlace( _email:string)
+{
+    const post = await prisma.place.update({
+        where: { id: 1 },
+        data: { email: _email },
+    })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
