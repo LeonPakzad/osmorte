9fbb7a4733ce8e779b829226fee2015e4656b1dc
LeonPakzad <44465654+LeonPakzad@users.noreply.github.com>
2024-04-10 10:14:25 +0200

    added basic navigation

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -2,12 +2,17 @@ const express = require('express');
 const {placeIndexView, placeView, placeEdit } = require('../controllers/placeController');
 const router = express.Router();
 
-router.get('/placeIndex', placeIndexView);
-router.get('/placeEdit', placeEdit);
+router.get('/place-index', placeIndexView);
+router.get('/place-edit', placeEdit);
 router.get('/place/:id', placeView);
 
 router.get('/', (_req: any, res: { render: (arg0: string, arg1: {}) => void; }) => {
     res.render("dashboard", {
+    } );
+});
+
+router.get('/about', (_req: any, res: { render: (arg0: string, arg1: {}) => void; }) => {
+    res.render("about", {
     } );
 });
 
